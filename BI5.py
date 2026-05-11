eed8f2767403320e6326ea73398c8da71f8bf832
a.patil16 <148852399+aditi24patil@users.noreply.github.com>
2026-05-11 18:05:54 +0530

    Update BI5.py

--- BI5.py
+++ BI5.py
@@ -1,3 +1,4 @@
+#Perform the data classification algorithm using any Classification algorithm.
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
